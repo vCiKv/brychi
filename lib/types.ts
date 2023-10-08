3fa841cbd1682b428b2a8b440b2e3690671cb39e
Kevin <54258753+vCiKv@users.noreply.github.com>
2023-10-08 20:42:46 +0100

    added:grouped all types

--- lib/types.ts
+++ lib/types.ts
@@ -1,16 +1,16 @@
-interface invoiceDataType{
+export interface invoiceDataType{
   id:string,
   drinks: DrinksType[],
   time: string,
 } 
 export interface DrinksType{
   drinkId:string;
-  cartonsAmount:number;
+  cartonsAmount:string;
   cost:number;
   sizeCl:number
 }
 export interface SellDataType extends invoiceDataType {
-  customerName:string | null
+  customerName?:string | null
 }
 export interface BuyDataType extends invoiceDataType {
   purchasedFrom: string,
@@ -22,5 +22,8 @@ export interface DrinkInventoryType {
   sizeCl: number;
   buyPrice: number;
   sellPrice: number;
-  inventory: number | string;
+  inventory: string;
+}
+export interface DrinkCardPropsType extends DrinksType {
+  drinkName: string;
 }
