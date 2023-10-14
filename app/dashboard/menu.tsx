f833a613d262e5e262b8c1c44e34952f90b2bf26
Kevin <54258753+vCiKv@users.noreply.github.com>
2023-10-14 12:56:14 +0100

    updated: menu icons and layout

--- app/dashboard/menu.tsx
+++ app/dashboard/menu.tsx
@@ -1,91 +1,24 @@
 "use client"
 import { cn } from "@/lib/utils";
+import { Home, Package, Percent, Truck } from "lucide-react";
 import { useRouter, useSearchParams } from "next/navigation";
+import { useEffect } from "react";
 
 const dashMenuItems = [
   {
-    icon: (
-      <svg
-        className="w-4 h-4 "
-        fill="none"
-        height="24"
-        stroke="currentColor"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth="2"
-        viewBox="0 0 24 24"
-        width="24"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
-        <polyline points="9 22 9 12 15 12 15 22" />
-      </svg>
-    ),
+    icon: <Home />,
     name: "home",
   },
   {
-    icon: (
-      <svg
-        className="w-4 h-4 "
-        fill="none"
-        height="24"
-        stroke="currentColor"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth="2"
-        viewBox="0 0 24 24"
-        width="24"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path d="M21 12V7H5a2 2 0 0 1 0-4h14v4" />
-        <path d="M3 5v14a2 2 0 0 0 2 2h16v-5" />
-        <path d="M18 12a2 2 0 0 0 0 4h4v-4Z" />
-      </svg>
-    ),
+    icon: <Percent />,
     name: "sales",
   },
   {
-    icon: (
-      <svg
-        className="w-4 h-4 "
-        fill="none"
-        height="24"
-        stroke="currentColor"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth="2"
-        viewBox="0 0 24 24"
-        width="24"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
-        <circle cx="9" cy="7" r="4" />
-        <path d="M22 21v-2a4 4 0 0 0-3-3.87" />
-        <path d="M16 3.13a4 4 0 0 1 0 7.75" />
-      </svg>
-    ),
+    icon: <Truck />,
     name: "orders",
   },
   {
-    icon: (
-      <svg
-        className="w-4 h-4 "
-        fill="none"
-        height="24"
-        stroke="currentColor"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth="2"
-        viewBox="0 0 24 24"
-        width="24"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path d="M2 9a3 3 0 0 1 0 6v2a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2v-2a3 3 0 0 1 0-6V7a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2Z" />
-        <path d="M13 5v2" />
-        <path d="M13 17v2" />
-        <path d="M13 11v2" />
-      </svg>
-    ),
+    icon: <Package />,
     name: "inventory",
   },
 ];
@@ -112,11 +45,11 @@ const DashboardMenu = () => {
       <button
         onClick={() => setActiveTab(props.index)}
         className={cn(
-          "flex items-center w-full px-2 py-2  gap-x-0 md:gap-x-2 text-gray-500 rounded-lg hover:bg-gray-200 active:bg-gray-300",
-          props.index === activeTab ? "bg-gray-300" : ""
+          "flex md:flex-row flex-col items-center w-full px-2 py-2  gap-x-0 md:gap-x-2 rounded-lg hover:bg-black/20 active:bg-black/40",
+          props.index === Number(activeTab) ? "bg-black/40" : ""
         )}
       >
-        <span className="hidden md:block">{props.icon}</span>
+        <span>{props.icon}</span>
 
         <span className="text-sm font-medium capitalize">{props.name}</span>
       </button>
@@ -124,8 +57,8 @@ const DashboardMenu = () => {
   };
 
   return (
-    <div className="fixed bottom-0 z-50 w-screen h-16 p-4 overflow-hidden text-gray-800 bg-gray-100">
-      <nav className="flex gap-1">
+    <div className="fixed bottom-0 z-50 w-screen h-20 p-4 overflow-hidden text-white bg-blue-800">
+      <nav className="flex gap-1 h-full">
         {dashMenuItems.map((menuItem, index) => (
           <MenuItem key={"menu-" + index} index={index} {...menuItem} />
         ))}
